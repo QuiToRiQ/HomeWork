8d5b3dac612e72efe1b7258b2adc3a4a0ba6823d
QuiToRiQ <140171628+QuiToRiQ@users.noreply.github.com>
2023-11-23 18:53:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function onclickLastUpdate(){
 function onClickCloseTab(){
     document.getElementById("UpdateList").style.display = "none";
 }
+function Description(text){
+    document.getElementById("descriptionNav").style.display = "block";
+    document.getElementById("textInDescription").innerHTML = text;
+}
+function closeDescriptionTab(){
+    document.getElementById("descriptionNav").style.display = "none";
+}
