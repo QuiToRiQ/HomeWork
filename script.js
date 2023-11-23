2d91379817c568555ddf4b26f80e1379d122cac0
QuiToRiQ <140171628+QuiToRiQ@users.noreply.github.com>
2023-11-23 23:17:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-
 function onclickLastUpdate(){
     document.getElementById("UpdateList").style.display = "block";
 }
 function onClickCloseTab(){
     document.getElementById("UpdateList").style.display = "none";
 }
-function Description(text){
-    document.getElementById("descriptionNav").style.display = "block";
-    document.getElementById("textInDescription").innerHTML = text;
-}
 function closeDescriptionTab(){
-    document.getElementById("descriptionNav").style.display = "none";
+    document.getElementById("Description").style.display = "none";
+}
+function Description(text = '', Uimg = ''){
+    closeDescriptionTab();
+    document.getElementById("textInDescription").innerText = text;
+    document.getElementById("imgInDescription").src=Uimg;
+    document.getElementById("Description").style.display = "block";
 }
